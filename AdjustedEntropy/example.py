7b54534ed119f5d60022ac7c805bf0a20428fa82
ldrbmrtv <baimuratov.i@gmail.com>
2021-05-19 23:57:52 +0300

    Update example.py

--- AdjustedEntropy/example.py
+++ AdjustedEntropy/example.py
@@ -1,68 +1,62 @@
 from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score as sil, davies_bouldin_score as db
-from sklearn.metrics import fowlkes_mallows_score
+from sklearn.metrics import fowlkes_mallows_score as fm
 import numpy as np
-from adj_entr import adj_entr
+from adjusted_entropy import adjusted_entropy as ae
 import matplotlib.pyplot as plt
 
 def cluster(X, k):
     model = KMeans(n_clusters=k, random_state=0).fit(X)
     return model.labels_
     
-def get_best_k(k_list, score_list, minmax=max):
+def get_best_k(k_list, scores, minmax=max):
     if max:
-        ind = np.argmax(score_list)
+        ind = np.argmax(scores)
     else:
-        ind = np.argmin(score_list)
+        ind = np.argmin(scores)
     best_k = k_list[ind]
     return best_k
 
-def cluster_and_evaluate(models, X, y, data_name, acc_metric):
+def evaluate(model, k_list, scores, score, score_name, minmax=max):
+    labels = cluster(X, get_best_k(k_list, scores, minmax))
+    model[score_name] = score(y, labels)
+
+def cluster_and_evaluate(models, X, y, score):
     n = len(X)
-    k_list = []
+    k_list = range(2, n)
     sil_list = []
     db_list = []
     ae_list = []
-    for k in range(2, n):
+    for k in k_list:
         labels = cluster(X, k)
-        k_list.append(k)
         sil_list.append(sil(X, labels))
         db_list.append(db(X, labels))
-        ae_list.append(adj_entr(labels))
+        ae_list.append(ae(labels))
         
     model = {}
-    
-    labels = cluster(X, get_best_k(k_list, sil_list))
-    model['sil_acc'] = acc_metric(y, labels)
-    
-    labels = cluster(X, get_best_k(k_list, db_list))
-    model['db_acc'] = acc_metric(y, labels)
-    
-    labels = cluster(X, get_best_k(k_list, ae_list))
-    model['ae_acc'] = acc_metric(y, labels)
-
+    evaluate(model, k_list, sil_list, score, 'sil')
+    evaluate(model, k_list, db_list, score, 'db', min)
+    evaluate(model, k_list, ae_list, score, 'ae')
     models.append(model)
 
+def present(models, k_list, score_name):
+    score = [x[score_name] for x in models]
+    print(np.mean(score))
+    plt.plot(k_list, score, label=score_name)
+    
 n = 150
+max_k = 16
 models = []
-k_list = []
-for k in range(2, 16):
+k_list = range(2, max_k)
+for k in k_list:
     print(k)
-    k_list.append(k)
     X, y = make_blobs(n_samples=n, n_features=3, centers=k, random_state=0)
-    cluster_and_evaluate(models, X, y, 'blobs', fowlkes_mallows_score)
+    cluster_and_evaluate(models, X, y, fm)
 
-sil_acc = [x['sil_acc'] for x in models]
-db_acc = [x['db_acc'] for x in models]
-ae_acc = [x['ae_acc'] for x in models]
+present(models, k_list, 'sil')
+present(models, k_list, 'db')
+present(models, k_list, 'ae')
 
-print(np.mean(sil_acc))
-print(np.mean(db_acc))
-print(np.mean(ae_acc))
-
-plt.plot(k_list, sil_acc, label='sil')
-plt.plot(k_list, db_acc, label='db')
-plt.plot(k_list, ae_acc, label='ae')
 plt.legend()
 plt.show()
